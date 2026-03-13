cf054f5f825cbcd13b87d37864301a6b200c3bb6
Yzxin <zhixin.25@intl.zju.edu.cn>
2026-03-13 11:31:34 +0800

    Final Version2.0

--- Practical4/creatine_clearance.py
+++ Practical4/creatine_clearance.py
@@ -1,22 +1,26 @@
 # Input age, weight, gender, and creatine concentration
-# check if gender is Female
-#          weight = weight * 0.85
-# check if age is not between 0 and 100
-#           print Please enter your age again
-# check if weight is not between 20 and 80
-#           print Please enter your age again
-# check if gender is not male or female
-#           print Please enter your gender again
-# check if creatine concentration is not between 0 and 100
-#           print Please enter your age again
+# check if gender is Female or Male
+#           if gender = Female : weight = weight * 0.85
+# check if age is between 0 and 100
+#           if not : re-input
+# check if weight is between 20 and 80
+#           if not : re-input
+# check if gender is male or female
+#           if not : re-input
+# check if creatine concentration is between 0 and 100
+#           if not : re-input
 # Calculate the CrCl
 # print CrCl
+
+
+# input variables
 age = int(input("Please enter your age(in years): "))
 weight = float(input("Please enter your weight(in kg): "))
 gender = input("Please enter your gender(M/F):")
 if gender == "F":
     weight *= 0.85
 creatine_concentration = float(input("Please enter your creatine concentration, Cr, in height (in µmol/l):"))
+# check if all the variables are within range.
 if age > 100 or age < 0:
     print("Please enter your age again")
 elif weight > 80 or weight < 20:

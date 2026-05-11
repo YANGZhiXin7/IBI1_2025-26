bfd7fd58f604970b12551e52a9abdb018e082402
YANGZhiXin7 <zhixin.25@intl.zju.edu.cn>
2026-05-11 21:57:59 +0800

    Enhance pie chart visualization for codon usage
    Updated pie chart plotting for codon usage with improved aesthetics and resolution.

--- Practical7/count_codons.py
+++ Practical7/count_codons.py
@@ -1,12 +1,9 @@
 import re
 import matplotlib.pyplot as plt
-
 # fasta file name
 filename = "Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa"
-
 # ask user for stop codon
 stop_codon = input("Enter a stop codon (TAA, TAG or TGA): ").upper()
-
 # check input
 if stop_codon != "TAA" and stop_codon != "TAG" and stop_codon != "TGA":
     print("Invalid input")
@@ -14,11 +11,11 @@ if stop_codon != "TAA" and stop_codon != "TAG" and stop_codon != "TGA":
 
 # read fasta file
 file = open(filename, "r")
-
+# parse fasta file into list of [header, sequence]
 sequences = []
 header = ""
 sequence = ""
-
+# read file line by line
 for line in file:
     line = line.strip()
 
@@ -29,21 +26,16 @@ for line in file:
         sequence = ""
     else:
         sequence = sequence + line.upper()
-
 # add the last sequence
 if header != "":
     sequences.append([header, sequence])
-
 file.close()
-
 # dictionary for codon counts
 codon_counts = {}
-
 # check each sequence
 for item in sequences:
     header = item[0]
     sequence = item[1]
-
     # regular expression for ORF
     pattern = "ATG(?:[ACGT]{3})*" + stop_codon
     matches = re.finditer(pattern, sequence)
@@ -77,14 +69,23 @@ else:
     for codon in sorted(codon_counts):
         print(codon, codon_counts[codon])
 
-    # make pie chart
+# plot pie chart
     labels = list(codon_counts.keys())
     sizes = list(codon_counts.values())
 
-    plt.figure(figsize=(10, 10))
-    plt.pie(sizes, labels=labels, autopct="%1.1f%%", labeldistance=1.1, pctdistance=0.8)
-    plt.title("Codon usage for stop codon " + stop_codon)
-    plt.savefig("codon_usage_" + stop_codon + ".png")
-    plt.close()
+    plt.figure(figsize=(14, 14), dpi=120)
 
-    print("Pie chart saved")
+    wedges, texts, autotexts = plt.pie(
+        sizes,
+        labels=labels,
+        autopct='%1.1f%%',
+        startangle=90,
+       labeldistance=1.1,
+       pctdistance=0.82,
+
+      textprops={'fontsize': 9},
+    )
+    plt.setp(autotexts, size=7, weight='bold')
+    plt.title('Codon usage for stop codon ' + stop_codon, fontsize=14, pad=20)
+    plt.savefig("codon_usage_" + stop_codon + ".png", bbox_inches='tight', dpi=300)
+    plt.close()

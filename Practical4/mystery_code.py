aea83144e1b1c51f04418e234d0f11b3d5f5123d
YANGZhiXin7 <zhixin.25@intl.zju.edu.cn>
2026-05-11 19:44:41 +0800

    Update comment to reflect generation of 11 numbers

--- Practical4/mystery_code.py
+++ Practical4/mystery_code.py
@@ -1,5 +1,5 @@
 # What does this piece of code do?
-# Answer: This code simulates generating 10 random numbers between 1 and 10 (inclusive) and calculates their total sum.
+# Answer: This code simulates generating 11 random numbers between 1 and 10 (inclusive) and calculates their total sum.
 # The variable total_rand stores the cumulative sum of the generated random numbers, while the variable progress counts how many numbers have been generated.
 # Once the loop finishes executing, the program prints the total sum of all the random numbers.
 
@@ -14,7 +14,7 @@ from math import ceil
 
 total_rand = 0 # This variable will store the total sum of the random numbers generated.
 progress=0 # This variable counts how many random numbers have been generated.
-while progress<=10: # The loop will continue until 10 random numbers have been generated
+while progress<=10: # The loop will continue until 11 random numbers have been generated
 	progress+=1 # count the number of random numbers generated
 	n = randint(1,10) # Generate a random number between 1 and 10
 	total_rand+=n # Add the generated random number to the total sum.
